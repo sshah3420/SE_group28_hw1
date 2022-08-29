8e8e50616dd2962e417a676a56aa86071849e274
Sanay Shah <sanayshah2@gmail.com>
2022-08-29 09:38:48 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='SE HW1',
+   version='1.0.0',
+   description='A module for a simple calculator',
+   author='Sanay Shah',
+   author_email='sshah34@ncsu.edu',
+   url='https://github.com/wutever0017/SE_group28_hw1',
+   packages=[],  #same as name
+)
