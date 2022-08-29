b01f4436331ed5dd1ae27f5aaecc86dffb6a7b47
Shaival Shah <31449664+sshah3420@users.noreply.github.com>
2022-08-29 09:28:00 -0400

    Delete calc_test.py

--- calc_test.py
+++
@@ -1,9 +0,0 @@
-import calculator as cr
-def test_add():
-    assert(cr.add(1,2)) == 3
-def test_subtract():
-    assert(cr.subtract(1,2)) == -1
-def test_multiply():
-    assert(cr.multiply(1,2)) == 2
-def test_divide():
-    assert(cr.divide(1,2)) == 0.5
