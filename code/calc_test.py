ded481eab07dfcac9940bc7c9d7f802b4d67b338
Shaival Shah <31449664+sshah3420@users.noreply.github.com>
2022-08-29 09:26:51 -0400

    Add files via upload

---
+++ code/calc_test.py
@@ -0,0 +1,9 @@
+import calculator as cr
+def test_add():
+    assert(cr.add(1,2)) == 3
+def test_subtract():
+    assert(cr.subtract(1,2)) == -1
+def test_multiply():
+    assert(cr.multiply(1,2)) == 2
+def test_divide():
+    assert(cr.divide(1,2)) == 0.5
